53031a78ce8ff72e756eb1affe21e7b844c41fdf
Ank-pro <ank.pro0115@gmail.com>
2025-01-26 22:37:49 +0530

    filter-1

--- frontend/src/components/Filter/FilterModal.jsx
+++ frontend/src/components/Filter/FilterModal.jsx
@@ -1,24 +1,163 @@
-import './filter.css'
+import { useEffect, useState } from "react";
+import "./filter.css";
+import RangeComponent from "./RangeComponent";
+import houseImg from "./assets/house.svg";
+import flatImg from "./assets/flat.svg";
+import guestImg from "./assets/guest.svg";
+import hotelImg from "./assets/hotel.svg";
+import { useSelector } from "react-redux";
 
 export const FilterModal = () => {
-    return <div className="filter-container">
-        <div className="price-range"></div>
-        <div className="rooms">
-            <ul className='rooms-category'>
-                <li>Bedroom</li>
-                <div className="range">
+  const [value, setValue] = useState([850, 20000]);
+  const { allHotels } = useSelector((state) => state.home);
+  const [filteredHotels, setFilteredHotels] = useState([]);
+  const [rooms, setRooms] = useState({
+    beds: 0,
+    bedrooms: 0,
+    bathrooms: 0,
+  });
 
-                </div>
-                <li>Bedroom</li>
-                <div className="range">
-                    
-                </div>
-                <li>Bedroom</li>
-                <div className="range">
-                    
-                </div>
-            </ul>
+  const { beds, bedrooms, bathrooms } = rooms;
+
+  useEffect(() => {
+    const hotels = allHotels.filter(
+      ({ price, numberOfBathrooms, numberOfBedrooms, numberOfBeds }) =>
+        price >= value[0] &&
+        price <= value[1] &&
+        (bathrooms === 0 || numberOfBathrooms === bathrooms) &&
+        (bedrooms === 0 || numberOfBedrooms === bedrooms) &&
+        (bedrooms === 0 || numberOfBeds === beds)
+    );
+    console.log(hotels);
+    setFilteredHotels(hotels);
+  }, [value, rooms]);
+
+  function handleUp(type) {
+    setRooms((prev) => {
+      return { ...prev, [type]: prev[type] + 1 };
+    });
+  }
+
+  function handleDown(type) {
+    setRooms((prev) => {
+      return { ...prev, [type]: prev[type] > 0 ? prev[type] - 1 : 0 };
+    });
+  }
+
+  function handleClear() {
+    setFilteredHotels([]);
+    setRooms({
+      beds: 0,
+      bedrooms: 0,
+      bathrooms: 0,
+    });
+    setValue([850, 20000]);
+  }
+
+  return (
+    <div className="filtering">
+      <div className="filter-header">
+        <div className="close">x</div>
+        <span className="filter-label">Filters</span>
+      </div>
+      <div className="filter-container">
+        <div className="price-range">
+          <RangeComponent value={value} setValue={setValue} />
+          <div className="prices">
+            <div className="amount">
+              <span className="amt-header">Minimum</span>
+              <span className="amt">{value[0]}</span>
+            </div>
+            <div className="amount">
+              <span className="amt-header">Maximum</span>
+              <span className="amt">{value[1]}</span>
+            </div>
+          </div>
         </div>
-        <div className="property-type"></div>
+        <hr />
+        <div className="rooms">
+          <label className="room-label">Beds and rooms</label>
+          <ul className="rooms-category">
+            <li className="room-header">Beds</li>
+            <div className="range">
+              <button
+                className="down"
+                disabled={false}
+                onClick={() => handleDown("beds")}
+              >
+                -
+              </button>
+              <span className="beds-value">
+                {rooms.beds === 0 ? "Any" : rooms.beds}
+              </span>
+              <button className="up" onClick={() => handleUp("beds")}>
+                +
+              </button>
+            </div>
+            <li className="room-header">Bedrooms</li>
+            <div className="range">
+              <button
+                className="down"
+                disabled={false}
+                onClick={() => handleDown("bedrooms")}
+              >
+                -
+              </button>
+              <span className="beds-value">
+                {rooms.bedrooms === 0 ? "Any" : rooms.bedrooms}
+              </span>
+              <button className="up" onClick={() => handleUp("bedrooms")}>
+                +
+              </button>
+            </div>
+            <li className="room-header">Bathroom</li>
+            <div className="range">
+              <button
+                className="down"
+                disabled={false}
+                onClick={() => handleDown("bathrooms")}
+              >
+                -
+              </button>
+              <span className="beds-value">
+                {rooms.bathrooms === 0 ? "Any" : rooms.bathrooms}
+              </span>
+              <button className="up" onClick={() => handleUp("bathrooms")}>
+                +
+              </button>
+            </div>
+          </ul>
+        </div>
+        <hr />
+        <div className="property-type">
+          <label className="property-label">Property Type</label>
+          <ul className="property-contents">
+            <li>
+              <img src={houseImg} alt="house" height="23" width="23" />
+              <span>House</span>
+            </li>
+            <li>
+              <img src={flatImg} alt="flat" height="23" width="23" />
+              <span>Flat</span>
+            </li>
+            <li>
+              <img src={hotelImg} alt="hotel" height="23" width="23" />
+              <span>Hotel</span>
+            </li>
+            <li>
+              <img src={guestImg} alt="guest-house" height="23" width="23" />
+              <span>Guest House</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="footer-result">
+        <button id="clear" onClick={handleClear}>Clear All</button>
+        <div className="filter-results">
+          Show {filteredHotels.length} places
+        </div>
+      </div>
     </div>
+  );
 };
---
+++ frontend/src/components/Filter/RangeComponent.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { Slider, Box, Typography } from "@mui/material";
+import "./filter.css";
+
+const RangeComponent = ({ value, setValue }) => {
+  //   const [value, setValue] = useState([850, 20000]);
+  const minPrice = 850;
+  const maxPrice = 20000;
+  const steps = 10;
+
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
+
+  const handleChange = (e, newValue) => {
+    setValue(newValue);
+  };
+
+  const renderTowers = () => {
+    const towers = [];
+    const range = maxPrice - minPrice;
+    const numberOfTowers = range / steps;
+
+    for (let i = 0; i <= numberOfTowers; i++) {
+      const currentValue = minPrice + i * steps;
+      const isActive =
+        currentValue >= value[0] && currentValue <= value[1] ? "active" : "";
+      towers.push(
+        <div
+          key={i}
+          className={`tower ${isActive}`}
+          style={{
+            height: `${(currentValue / maxPrice) * 100}%`,
+            left: `${(i / numberOfTowers) * 100}%`,
+          }}
+        ></div>
+      );
+    }
+    return towers;
+  };
+
+  return (
+    <Box
+      sx={{
+        maxWidth: 525,
+        margin: "auto",
+        position: "relative",
+        // border : '1px solid black',
+      }}
+    >
+      <Typography
+        variant="h6"
+        sx={{display: "inline" }}
+      >
+        Price Range
+      </Typography>
+
+      <div className="slider-container">
+        <Slider
+          value={value}
+          onChange={handleChange}
+          min={minPrice}
+          max={maxPrice}
+          step={steps}
+          valueLabelDisplay="auto"
+          sx={{
+            color: "rgb(117, 201, 112)",
+            "& .MuiSlider-thumb": {
+              width: 15,
+              height: 15, 
+            },
+            "& .MuiSlider-track": {
+              height: 4, 
+            },
+            "& .MuiSlider-rail": {
+              height: 4, 
+            },
+          }}
+        />
+        <div className="towers">{renderTowers()}</div>
+      </div>
+    </Box>
+  );
+};
+
+export default RangeComponent;
